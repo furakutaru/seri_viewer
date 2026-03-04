c70db3a4c582d537b02989d17f298a6f7a6896ee
furakutaru <ec.yuma@gmail.com>
2026-03-04 20:31:38 +0900

    エクスポートの不正対応

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-// This file is a placeholder for the Vercel Serverless Function.
-// It will be overwritten by 'npm run build:server' during deployment.
-export default {
-    handler: (req, res) => res.status(500).send("Build failed to overwrite this file.")
+import app from "../dist/server.js";
+
+export default (req, res) => {
+    return app(req, res);
 };
